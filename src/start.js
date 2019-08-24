9b78385080b6c9c173fa9e68273ada1fc267ae3d
Bryan Goldstein <brysgo@gmail.com>
2019-08-23 23:01:12 -0400

    switch to one start command

---
+++ src/start.js
@@ -0,0 +1,8 @@
+import { delegateToPlugin } from "./delegateToPlugin";
+
+export default delegateToPlugin({
+  command: "start",
+  aliases: [],
+  desc: "start the any cloud service with the selected runtime plugin",
+  builder: yargs => {}
+});
