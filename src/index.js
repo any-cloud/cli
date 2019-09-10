fa7a09b96690dbaa9ac3109fbf33f95f370b5a0a
Bryan Goldstein <brysgo@gmail.com>
2019-09-10 11:48:47 -0400

    0.0.5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-require("yarg-scripts/index.js");
+const argv = require("yargs");
+
+// delegate to scripts directory for command pattern
+const normalizedPath = require("path").join(__dirname, "./scripts");
+require("fs")
+  .readdirSync(normalizedPath)
+  .reduce(function(acc, file) {
+    const args = require("./scripts/" + file);
+    return acc.command(args.default || args);
+  }, argv)
+  .help().argv;
