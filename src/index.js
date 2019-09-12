dcf920b5fd0d21b9c8cb3f08c2a72a6f74879046
Bryan Goldstein <brysgo@gmail.com>
2019-09-12 11:37:29 -0400

    support plugin defined commands

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 
-const argv = require("yargs");
+const yargs0 = require("yargs");
+const fs = require("fs");
+const path = require("path");
+const {
+  utils: { currentPluginCLIHandlerPath }
+} = require("@any-cloud/core");
 
 // delegate to scripts directory for command pattern
-const normalizedPath = require("path").join(__dirname, "./scripts");
-require("fs")
-  .readdirSync(normalizedPath)
-  .reduce(function(acc, file) {
-    const args = require("./scripts/" + file);
-    return acc.command(args.default || args);
-  }, argv)
-  .help().argv;
+const pluginCliScriptsPath = currentPluginCLIHandlerPath();
+const yargs1 = fs.readdirSync(pluginCliScriptsPath).reduce(function(acc, file) {
+  const args = require(path.join(pluginCliScriptsPath, file));
+  return acc.command(args.default || args);
+}, yargs0);
+const cliScriptsPath = path.join(__dirname, "./scripts");
+const yargs2 = fs.readdirSync(cliScriptsPath).reduce(function(acc, file) {
+  const args = require(path.join(cliScriptsPath, file));
+  return acc.command(args.default || args);
+}, yargs1);
+yargs2.help().argv;
