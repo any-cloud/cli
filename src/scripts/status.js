af7e225be3bdb922412dd71ed5963b18045fb298
Bryan Goldstein <brysgo@gmail.com>
2019-09-11 21:54:07 -0400

    add support for switching selected plugin

--- src/scripts/status.js
+++ src/scripts/status.js
@@ -1,9 +1,12 @@
+import { utils } from "@any-cloud/core";
+
 export default {
   command: "status",
   aliases: ["s"],
   desc: "get the currently selected plugin and other info",
   builder: yargs => {},
-  handler: argv => {
-    console.log("not yet implemented");
+  handler: async argv => {
+    console.log("current plugin is set to: ", await utils.currentPlugin());
+    process.exit(0);
   }
 };
--- src/scripts/use.js
+++ src/scripts/use.js
@@ -1,9 +1,13 @@
+import { utils } from "@any-cloud/core";
+
 export default {
-  command: "use",
+  command: "use plugin",
   aliases: ["u"],
   desc: "change which plugin is selected",
   builder: yargs => {},
-  handler: argv => {
-    console.log("not yet implemented");
+  handler: async argv => {
+    utils.setCurrentPlugin(argv.plugin);
+    console.log("set plugin to: ", await utils.currentPlugin());
+    process.exit(0);
   }
 };
