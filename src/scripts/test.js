f9d50527646178caf5e6ef563813f767cf5f3980
Bryan Goldstein <brysgo@gmail.com>
2019-09-21 00:07:20 -0400

    don't need to any-cloud use in test script anymore

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,23 +1,16 @@
-import { utils } from "@any-cloud/core";
+import { spawn } from "child_process";
 import path from "path";
-const { spawn, spawnSync } = require("child_process");
+
 export default {
   command: "test",
   aliases: ["t"],
   desc: "test the current plugin's api compliance",
   builder: yargs => {},
   handler: async argv => {
-    const currentPlugin = await utils.currentPlugin();
-    console.log(`testing '${currentPlugin}' for api compliance`);
     const spawnOpts = {
       cwd: path.join(__dirname, "../../"),
       env: process.env
     };
-    spawnSync(
-      "yarn",
-      ["exec", "any-cloud", "use", currentPlugin.split("@any-cloud/")[1]],
-      spawnOpts
-    );
     const child = spawn("yarn", ["test", ...process.argv.slice(3)], spawnOpts);
 
     child.stdout.pipe(process.stdout);
