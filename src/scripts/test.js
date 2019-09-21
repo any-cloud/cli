f339fb2bd50dee8c2533c620ea5f1c21db6acc1e
Bryan Goldstein <brysgo@gmail.com>
2019-09-20 22:43:43 -0400

    v0.0.9

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,17 +1,24 @@
 import { utils } from "@any-cloud/core";
 import path from "path";
-const { spawn } = require("child_process");
+const { spawn, spawnSync } = require("child_process");
 export default {
   command: "test",
   aliases: ["t"],
   desc: "test the current plugin's api compliance",
   builder: yargs => {},
   handler: async argv => {
-    console.log(`testing '${await utils.currentPlugin()}' for api compliance`);
-    const child = spawn("yarn", ["test", ...process.argv.slice(3)], {
+    const currentPlugin = await utils.currentPlugin();
+    console.log(`testing '${currentPlugin}' for api compliance`);
+    const spawnOpts = {
       cwd: path.join(__dirname, "../../"),
       env: process.env
-    });
+    };
+    spawnSync(
+      "yarn",
+      ["exec", "any-cloud", "use", currentPlugin.split("@any-cloud/")[1]],
+      spawnOpts
+    );
+    const child = spawn("yarn", ["test", ...process.argv.slice(3)], spawnOpts);
 
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);
